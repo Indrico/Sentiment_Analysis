49d05b60e3738de691961225d326f5b04a7c81c3
Indrico <Indrico.jowensen@gmail.com>
2020-06-01 23:34:22 +0700

    Change static file settings

--- Sentiment_Analysis/settings.py
+++ Sentiment_Analysis/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
