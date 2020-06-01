787b7c4e749a9a28d01c1d8d732ac2cd7476cce0
Indrico <Indrico.jowensen@gmail.com>
2020-06-01 23:42:21 +0700

    Add static file

--- Sentiment_Analysis/settings.py
+++ Sentiment_Analysis/settings.py
@@ -50,6 +50,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
 ROOT_URLCONF = 'Sentiment_Analysis.urls'
 
 TEMPLATES = [
@@ -120,6 +126,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
